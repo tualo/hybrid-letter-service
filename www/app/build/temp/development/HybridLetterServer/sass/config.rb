cba8cc6194acb385361ff4abeb90a734c616dbba
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2018-12-10 13:17:48 +0100

    add build

---
+++ www/app/build/temp/development/HybridLetterServer/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/Users/thomashoffmann/Documents/Projects/sencha/hybrid-letter-server/www/app/build/.sass-cache'
---
+++ www/app/build/temp/development/HybridLetterServer/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/thomashoffmann/Documents/Projects/sencha/hybrid-letter-server/www/app/app.js
